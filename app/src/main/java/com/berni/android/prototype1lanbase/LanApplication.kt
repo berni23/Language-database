1bc56bebab3e5ad7d3582b6dfa88e5a1c1c55fa8
Bernat Ferrer <bernatferrerm@gmail.com>
2020-04-14 19:49:30 +0200

    a lot of changes, bug in nav_graph.xml, line 28 ( arguments)

--- app/src/main/java/com/berni/android/prototype1lanbase/LanApplication.kt
+++ app/src/main/java/com/berni/android/prototype1lanbase/LanApplication.kt
@@ -1,22 +1,23 @@
-package com.berni.android.prototype1lanbase
-
 import android.app.Application
 import com.berni.android.prototype1lanbase.db.LanDataBase
 import com.berni.android.prototype1lanbase.ui.ViewModelFactory
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
 import org.kodein.di.generic.bind
 import org.kodein.di.generic.instance
 import org.kodein.di.generic.provider
 import org.kodein.di.generic.singleton
+import java.util.*
 
 class LanApplication() : Application(), KodeinAware {
 
-
            override val kodein = Kodein.lazy {
 
+           import(androidXModule(this@LanApplication))
+
             bind() from singleton { LanDataBase(instance()) }
-            bind() from singleton { instance <LanDataBase>().CatDao() }
+            bind() from singleton { instance <LanDataBase>().catDao() }
 
           //  bind<ForecastRepository>() with singleton { ForecastRepositoryImpl(instance(),instance()) }
             bind() from provider{ ViewModelFactory(instance()) }
