a9b925c6d991a20672f55f1c8406168e03c665a4
Bernat Ferrer <bernatferrerm@gmail.com>
2020-04-19 18:23:35 +0200

    menu options linked with clickListener

--- app/src/main/java/com/berni/android/prototype1lanbase/ui/CatAdapter.kt
+++ app/src/main/java/com/berni/android/prototype1lanbase/ui/CatAdapter.kt
@@ -1,17 +1,15 @@
 package com.berni.android.prototype1lanbase.ui
 
 import android.view.*
-import android.widget.PopupWindow
+import android.view.ContextMenu.ContextMenuInfo
+import android.widget.AdapterView
 import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.navigation.Navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.berni.android.prototype1lanbase.R
-import com.berni.android.prototype1lanbase.databinding.FragmentFirstBinding.inflate
 import com.berni.android.prototype1lanbase.db.Cat
 import kotlinx.android.synthetic.main.adapter_cat.view.*
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.Date.from
 
 
 class CatAdapter(private val cats: List<Cat>) : RecyclerView.Adapter<CatAdapter.CatViewHolder>(),
@@ -24,12 +22,19 @@ class CatAdapter(private val cats: List<Cat>) : RecyclerView.Adapter<CatAdapter.
         )
     }
 
+
+    override fun onCreateContextMenu(menu: ContextMenu, v: View?, menuInfo: ContextMenuInfo?) {
+
+        MenuInflater(v?.context).inflate(R.menu.menu_cat, menu)
+
+    }
+
+
     override fun getItemCount() = cats.size
 
-    override fun onBindViewHolder(holder: CatViewHolder, position: Int)  {
+    override fun onBindViewHolder(holder: CatViewHolder, position: Int) {
 
         holder.view.setOnCreateContextMenuListener(this)
-
         holder.view.text_view_title.text = cats[position].catName
         holder.view.text_view_date.text = cats[position].catDate
 
@@ -39,27 +44,33 @@ class CatAdapter(private val cats: List<Cat>) : RecyclerView.Adapter<CatAdapter.
             val bundle = bundleOf("categoryName" to cats[position].catName)
 
             findNavController(it).navigate(R.id.actionAddCat, bundle)
-
         }
 
 
-        fun onContextItemSelected( item: MenuItem) {
+        holder.view.setOnCreateContextMenuListener { menu: ContextMenu?, v: View?, menuInfo: ContextMenuInfo? ->
 
-             when (item.itemId) {
+            val info = menuInfo as AdapterView.AdapterContextMenuInfo
 
-                 R.id.delete -> Toast.makeText(holder.view.context,"deleting..",Toast.LENGTH_SHORT).show()
 
-                 R.id.rename ->  Toast.makeText(holder.view.context,"deleting..",Toast.LENGTH_SHORT).show()
-             }
+            menu?.add("delete")?.setOnMenuItemClickListener {
+                Toast.makeText(v?.context, "deleting..", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+
+            menu?.add("rename")?.setOnMenuItemClickListener {
+                Toast.makeText(v?.context, "renaming..", Toast.LENGTH_SHORT).show()
+                true
+            }
+
         }
-
     }
 
-    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
 
-        MenuInflater(v?.context).inflate(R.menu.menu_cat, menu);
+    class CatViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
 }
-    class CatViewHolder(val view: View) : RecyclerView.ViewHolder(view)
-    }
+
+
+
 
