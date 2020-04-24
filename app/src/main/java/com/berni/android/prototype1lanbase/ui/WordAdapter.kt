c4d0c583b1db69d30a611f06f00648879bf1d5e9
Bernat Ferrer <bernatferrerm@gmail.com>
2020-04-24 12:43:46 +0200

    version beta for seach bar

--- app/src/main/java/com/berni/android/prototype1lanbase/ui/WordAdapter.kt
+++ app/src/main/java/com/berni/android/prototype1lanbase/ui/WordAdapter.kt
@@ -4,16 +4,27 @@ import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.berni.android.prototype1lanbase.R
 import com.berni.android.prototype1lanbase.db.Word
 import kotlinx.android.synthetic.main.adapter_word.view.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.coroutines.CoroutineContext
 
 
 // WordInCat
 
-class WordAdapter(private val words: List<Word>): RecyclerView.Adapter<WordAdapter.WordViewHolder>()
+class WordAdapter(private val words: List<Word>, override val coroutineContext: CoroutineContext): RecyclerView.Adapter<WordAdapter.WordViewHolder>(),Filterable,
+    CoroutineScope
 {
+
+    var wordNameList:MutableList<String>? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
 
@@ -21,16 +32,60 @@ class WordAdapter(private val words: List<Word>): RecyclerView.Adapter<WordAdapt
 
         }
 
-    override fun getItemCount() = words.size?:0
+    override fun getItemCount() = words.size
 
     @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: WordViewHolder, position: Int) {
 
-        holder.view.text_view_word.text = " ${words.get(position).wordName} "  // adding space at the beginning and at the end to avoid text cut
-        holder.view.text_view_translation.text = " ${words.get(position).trans1} "
+        wordNameList?.add(words[position].wordName)
+
+        holder.view.text_view_word.text = " ${words[position].wordName} "  // adding space at the beginning and at the end to avoid text cut
+        holder.view.text_view_translation.text = " ${words[position].trans1} "
 
     }
 
     class WordViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    override fun getFilter(): Filter {
+
+        return myFilter
+    }
+
+    private var myFilter: Filter = object : Filter() {
+        //Automatic on background thread
+        override fun performFiltering(charSequence: CharSequence): FilterResults {
+
+            val filteredList: MutableList<String> = ArrayList()
+            if (charSequence.isEmpty()) {
+                filteredList.addAll(wordNameList!!)
+            } else {
+                for (word in wordNameList!!) {
+                    if (word.toLowerCase(Locale.ROOT).contains(charSequence.toString().toLowerCase(
+                            Locale.ROOT
+                        )
+                        )
+                    ) {
+                        filteredList.add(word)
+                    }
+                }
+            }
+            val filterResults = FilterResults()
+            filterResults.values = filteredList
+            return filterResults
+        }
+
+        //Automatic on UI thread
+        override fun publishResults(
+
+            charSequence: CharSequence,
+            filterResults: FilterResults
+        ) {
+            
+            wordNameList?.clear()
+            wordNameList?.addAll(filterResults.values as Collection<String>)
+            notifyDataSetChanged()
+        }
+    }
+
 
 }
--- app/src/main/java/com/berni/android/prototype1lanbase/ui/WordsListFragment.kt
+++ app/src/main/java/com/berni/android/prototype1lanbase/ui/WordsListFragment.kt
@@ -3,22 +3,20 @@ package com.berni.android.prototype1lanbase.ui
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.*
-import android.widget.Adapter
+import android.widget.SearchView
 import android.widget.Toast
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.berni.android.prototype1lanbase.R
 import com.berni.android.prototype1lanbase.db.Word
 import kotlinx.android.synthetic.main.fragment_words_list.*
-import kotlinx.coroutines.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.closestKodein
 import org.kodein.di.generic.instance
-import kotlinx.android.synthetic.*
 
 /**
  * A simple [Fragment] subclass.
@@ -29,8 +27,8 @@ class WordsListFragment : BaseFragment(), KodeinAware {
 
     lateinit var categoryName: String
     lateinit var lastAdded: List<Word?>
-    var lastAdditionDate : String? = ""
 
+    var lastAdditionDate : String? = ""
 
     var adapter : WordAdapter? = null
 
@@ -57,45 +55,32 @@ class WordsListFragment : BaseFragment(), KodeinAware {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        lateinit var wordsInCat: List<Word>
+
         recycler_view_words.setHasFixedSize(true)
-
-        recycler_view_words.layoutManager =
-            LinearLayoutManager(context, RecyclerView.VERTICAL, false)
-
-        // navController = Navigation.findNavController(view)xt
+        recycler_view_words.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
 
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
 
-        runBlocking(Dispatchers.Default) {
+        runBlocking(Dispatchers.Default) { wordsInCat= viewModel.wordsInCat(categoryName).reversed() }
 
-            val wordsInCat: List<Word> = viewModel.wordsInCat(categoryName).reversed()
-            adapter = WordAdapter(wordsInCat) // sorting by last added
-            recycler_view_words.adapter = adapter
-            numWords = adapter!!.itemCount
+        adapter = WordAdapter(wordsInCat,coroutineContext) // sorting by last added
 
-            lastAdded =
-                listOf(wordsInCat.getOrNull(0), wordsInCat.getOrNull(1), wordsInCat.getOrNull(2))
-            lastAdditionDate = wordsInCat.getOrNull(0)?.date
-        }
+        recycler_view_words.adapter = adapter
+        numWords = adapter!!.itemCount
+
+        lastAdded = listOf(wordsInCat.getOrNull(0), wordsInCat.getOrNull(1), wordsInCat.getOrNull(2))
+        lastAdditionDate = wordsInCat.getOrNull(0)?.date
 
         var lastAdditions = "Last additions: "
 
-        lastAdded.forEach {
-
-            if (it?.wordName != null) {
-
-                lastAdditions += " ${it.wordName},"
-
-            }
-        }
+        lastAdded.forEach {   if (it?.wordName != null) {   lastAdditions += " ${it.wordName},"    }}
 
         lastAdditions = lastAdditions.dropLast(1)
 
         //   Toast.makeText(view.context, " word = ${it?.wordName}", Toast.LENGTH_SHORT).show() }
 
-        if (lastAdded.elementAt(0)?.wordName == null) {
-            lastAdditions = "No words added yet"
-        }
+        if (lastAdded.elementAt(0)?.wordName == null) {  lastAdditions = "No words added yet"  }
 
         val stringLastAdditionDate = "Last addition on $lastAdditionDate"
         text_view_numWords.text = " ${numWords.toString()} words"
@@ -106,10 +91,32 @@ class WordsListFragment : BaseFragment(), KodeinAware {
 
         override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
 
-               super.onCreateOptionsMenu(menu, inflater)
-               inflater.inflate(R.menu.menu_words, menu)
 
+            inflater.inflate(R.menu.menu_words, menu)
+
+            val searchItem = menu.findItem(R.id.item_search)
+            val searchView = searchItem.actionView as SearchView
+            searchView.queryHint = "Search word"
+
+            searchView.setOnQueryTextListener(object :
+                SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String): Boolean {
+
+                    adapter!!.filter.filter(newText)
+
+                    recycler_view_words.adapter = adapter
+
+                    return false
+                }
+            })
+
+            return super.onCreateOptionsMenu(menu, inflater)
     }
+
 
         override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
@@ -121,7 +128,7 @@ class WordsListFragment : BaseFragment(), KodeinAware {
 
                 runBlocking(Dispatchers.Default){sorted = viewModel.wordsInCatAlphabetic(categoryName)}
 
-                    recycler_view_words.adapter = WordAdapter(sorted!!)
+                    recycler_view_words.adapter = WordAdapter(sorted!!,coroutineContext)
 
                     Toast.makeText(context, "sorting by alphabetic order..", Toast.LENGTH_SHORT).show()}
 
@@ -129,36 +136,31 @@ class WordsListFragment : BaseFragment(), KodeinAware {
             R.id.last_added ->{
 
                runBlocking(Dispatchers.Default){sorted = viewModel.wordsInCat(categoryName).reversed()}
-                    recycler_view_words.adapter = WordAdapter(sorted!!)
+                    recycler_view_words.adapter = WordAdapter(sorted!!,coroutineContext)
 
                 Toast.makeText(context, "sorting by last added..", Toast.LENGTH_SHORT).show() }
 
-
             R.id.first_added ->{
 
-
                 runBlocking(Dispatchers.Default){sorted = viewModel.wordsInCat(categoryName)}
-                recycler_view_words.adapter = WordAdapter(sorted!!)
+                recycler_view_words.adapter = WordAdapter(sorted!!,coroutineContext)
 
                 Toast.makeText(context, "sorting by first added..", Toast.LENGTH_SHORT).show() }
-
 
             R.id.withExample ->{
 
 
                 runBlocking(Dispatchers.Default){sorted = viewModel.filterExample(categoryName)}
-                recycler_view_words.adapter = WordAdapter(sorted!!)
+                recycler_view_words.adapter = WordAdapter(sorted!!,coroutineContext)
 
                 Toast.makeText(context, "filtering words with an example..", Toast.LENGTH_SHORT).show() }
-
 
             R.id.noExample ->{
 
                 runBlocking(Dispatchers.Default){sorted = viewModel.filterNoExample(categoryName)}
-                recycler_view_words.adapter = WordAdapter(sorted!!)
+                recycler_view_words.adapter = WordAdapter(sorted!!,coroutineContext)
 
                     Toast.makeText(context, "filtering words without example..", Toast.LENGTH_SHORT).show() }
-
         }
 
         return super.onOptionsItemSelected(item)
