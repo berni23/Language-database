5c98fd09fa589a48ad5291a50db693109b33d83c
Bernat Ferrer <bernatferrerm@gmail.com>
2020-05-24 18:29:33 +0200

    statistics algorithm upgraded

--- app/src/main/java/com/berni/android/prototype1lanbase/ui/Statistics3Fragment.kt
+++ app/src/main/java/com/berni/android/prototype1lanbase/ui/Statistics3Fragment.kt
@@ -3,6 +3,7 @@ package com.berni.android.prototype1lanbase.ui
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.activity.OnBackPressedCallback
 import kotlinx.android.synthetic.main.fragment_statistics3.*
@@ -14,13 +15,17 @@ import com.anychart.AnyChart
 import com.anychart.chart.common.dataentry.DataEntry
 import com.anychart.chart.common.dataentry.ValueDataEntry
 import com.berni.android.prototype1lanbase.R
+import com.jakewharton.threetenabp.AndroidThreeTen
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import org.kodein.di.KodeinAware
 import org.threeten.bp.LocalDateTime
 import org.kodein.di.android.x.closestKodein
 import org.kodein.di.generic.instance
+import org.threeten.bp.LocalDate
+import org.threeten.bp.format.DateTimeFormatter
 import org.threeten.bp.temporal.ChronoUnit
+import java.lang.Math.abs
 import kotlin.collections.ArrayList
 
 class Statistics3Fragment : BaseFragment(),KodeinAware {
@@ -42,49 +47,34 @@ class Statistics3Fragment : BaseFragment(),KodeinAware {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        var date1 = LocalDateTime.now()
-        var date2 = date1.plusDays(-3)
-        println(date1.until(date2, ChronoUnit.DAYS))
-
-
 
         navController = Navigation.findNavController(view)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
         APIlib.getInstance().setActiveAnyChartView(lineChart)
 
         val lineDays = AnyChart.line()
-        val dataDays = dataEntries(sortDays())
-
+        val dataDays = arrayDays(sortDays())
         lineDays.data(dataDays)
         lineChart.setChart(lineDays)
 
-        changeGraphs.setOnClickListener{ navController.navigate(R.id.actionMonthlyView) }
+        changeGraphs.setOnClickListener { navController.navigate(R.id.actionMonthlyView) }
         super.onViewCreated(view, savedInstanceState)
 
 
-        }
-
-
-    private fun sortDays(): MutableList<String> {
-
-        var days = mutableListOf<String>()
-        runBlocking(Dispatchers.Default) { days = viewModel.orderDays() }
-        days.sort()
-        days.sortBy { it.substring(3, 5).toInt() }
-        days.sortBy { it.substring(6, 10).toInt() }
-        return days
     }
 
-     private fun dataEntries(days: List<String>): ArrayList<DataEntry> {
 
-            val dataDays = ArrayList<DataEntry>()
-            var counter = days.groupingBy { it }.eachCount().toList()
-            if (counter.size > 100) {  counter = counter.takeLast(100) }
-            counter.toMap()
-            counter.forEach {dataDays.add(ValueDataEntry(it.first, it.second)) }
-            return dataDays
+    private fun sortDays(): ArrayList<String> {
 
-        }
+        var days = ArrayList<String>()
+        runBlocking(Dispatchers.Default) {days = viewModel.orderDays() as ArrayList<String> }
+        days.sort()
+        days.sortBy {it.substring(3, 5).toInt()}
+        days.sortBy {it.substring(6, 10).toInt()}
+
+        Log.println(Log.INFO,"days",days.toString())
+        return days
+    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -101,7 +91,6 @@ class Statistics3Fragment : BaseFragment(),KodeinAware {
         )
     }
 
-
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.menu_s3, menu)
         return super.onCreateOptionsMenu(menu, inflater)
@@ -109,12 +98,48 @@ class Statistics3Fragment : BaseFragment(),KodeinAware {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-        when (item.itemId) {R.id.item_backS3 -> {navController.popBackStack()} }
-
+        when (item.itemId) {
+            R.id.item_backS3 -> {
+                navController.popBackStack()
+            }
+        }
         return super.onOptionsItemSelected(item)
     }
-}
 
+    private fun arrayDays(days: ArrayList<String>): ArrayList<DataEntry> {
+
+        AndroidThreeTen.init(activity)
+        val today = LocalDate.now()
+        val xAxis = ArrayList<String>()
+        val format1 = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+        val firstDay = LocalDate.parse(days[0], format1)
+        val dataDays = ArrayList<DataEntry>()
+        var daysPassed = abs(today.until(firstDay, ChronoUnit.DAYS))
+
+        Log.println(Log.INFO,"daysPassed",daysPassed.toString())
+
+        if (daysPassed >100) {daysPassed = 100}
+        if(daysPassed.toInt()==0) {
+
+            xAxis.add(format1.format(today))
+            dataDays.add(ValueDataEntry(xAxis[0],days.count{it == xAxis[0]}))
+
+        }
+
+        else {
+
+            for (i in 0..daysPassed) {
+                xAxis.add(format1.format(today.plusDays(-i)))
+            }
+
+            xAxis.reversed()
+            for (i in 0..xAxis.size) { dataDays.add(ValueDataEntry(xAxis[i], days.count { it == xAxis[i] })) }
+        }
+
+        Log.println(Log.INFO,"xAxis.size",xAxis.size.toString())
+        return dataDays
+    }
+}
 
 
 
