727897af2527c6685c8eb906a5a0826462ed442b
berni23 <bernatferrerm@gmail.com>
2020-06-05 17:07:56 +0200

    back button for tutorial added

--- app/src/main/java/com/berni/android/prototype1lanbase/ui/TutorialActivity.kt
+++ app/src/main/java/com/berni/android/prototype1lanbase/ui/TutorialActivity.kt
@@ -10,65 +10,80 @@ import kotlinx.android.synthetic.main.activity_tutorial.*
 class TutorialActivity : AppCompatActivity() {
 
     var msg: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tutorial)
         setSupportActionBar(findViewById(R.id.toolbar))
         supportActionBar?.title = " Language Database"
 
-        btn_tutorial_next.setOnClickListener {
-
-            if (msg == 0) {
-
-                msg++
-                tutorial_t1.text = resources.getString(R.string.tutorial_m3)
-                tutorial_t2.text = ""
-                tutorial_t3.text = ""
-
-            } else if (msg == 1) {
-                msg++
-
-                tutorial_t1.text = resources.getString(R.string.tutorial_m4)
-                tutorial_t2.text = resources.getString(R.string.tutorial_m5)
-
-            } else if (msg == 2) {
-                msg++
-                tutorial_t1.text = resources.getString(R.string.tutorial_m6)
-                tutorial_t2.text = resources.getString(R.string.tutorial_m7)
-
-            } else if (msg == 3) {
-
-                msg++
-
-                tutorial_t1.text = resources.getString(R.string.tutorial_m8)
-                tutorial_t2.text = ""
-
-            } else if (msg == 4) {
-
-                msg++
-                tutorial_t1.text = resources.getString(R.string.tutorial_m9)
-
-
-            } else if (msg == 5) {
-
-                msg++
-                tutorial_t1.text = ""
-                tutorial_t2.text = ""
-                tutorial_t3.text = resources.getString(R.string.tutorial_m10)
-                tutorial_t3.textSize = 30F
-            } else {
-
-                Tutorial.firstTime = false
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-
-            }
-        }
+        btn_tutorial_next.setOnClickListener { messages(1 ) }
+        btn_tutorial_back.setOnClickListener { messages(-1) }
     }
 
     override fun onBackPressed() {
         finish()
     }
+
+   private fun messages(count:Int) {
+
+    if (msg == 0) {
+
+
+        if (count>0) {
+
+            msg += count
+            tutorial_t1.text = resources.getString(R.string.tutorial_m3)
+            tutorial_t2.text = ""
+            tutorial_t3.text = ""
+
+        }
+
+    } else if (msg == 1) {
+
+        msg+=count
+        tutorial_t1.text = resources.getString(R.string.tutorial_m4)
+        tutorial_t2.text = resources.getString(R.string.tutorial_m5)
+
+    } else if (msg == 2) {
+        msg+=count
+        tutorial_t1.text = resources.getString(R.string.tutorial_m6)
+        tutorial_t2.text = resources.getString(R.string.tutorial_m7)
+
+    } else if (msg == 3) {
+
+        msg+=count
+
+        tutorial_t1.text = resources.getString(R.string.tutorial_m8)
+        tutorial_t2.text = ""
+
+    } else if (msg == 4) {
+
+        msg+=count
+        tutorial_t1.text = resources.getString(R.string.tutorial_m9)
+
+
+    } else if (msg == 5) {
+
+        msg += count
+        tutorial_t1.text = ""
+        tutorial_t2.text = ""
+        tutorial_t3.text = resources.getString(R.string.tutorial_m10)
+        tutorial_t3.textSize = 30F
+
+    }
+
+        //else if (msg<0) { msg=0 }
+
+     else {
+
+        Tutorial.firstTime = false
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+
+        }
+   }
 }
 
 
